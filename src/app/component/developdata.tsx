d2d90640f0166cc3e7f5cd905cfaccf30cf03cec
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-04-09 13:14:13 +0530

    SideBar Dropdown done

---
+++ src/app/component/developdata.tsx
@@ -0,0 +1,31 @@
+
+const developerData = [
+    {
+      id: 1,
+      img: './api.svg',
+      name: "API Reference"
+    },
+    {
+        id: 2,
+        img: './api.svg',
+        name: "Widget Reference"
+      },
+      {
+        id: 3,
+        img: './api.svg',
+        name: "Integrations"
+      },
+      {
+        id: 4,
+        img: './api.svg',
+        name: "Theme Designer"
+      },
+      {
+        id: 5,
+        img: './api.svg',
+        name: "Technology"
+      },
+      
+  ];
+  export default developerData;
+  
---
+++ src/app/component/resourcesdata.tsx
@@ -0,0 +1,40 @@
+
+const Resources = [
+    {
+      id: 1,
+      img: './themes.svg',
+      name: "Themes"
+    },
+    {
+        id: 2,
+        img: './themes.svg',
+        name: "About Us"
+      },
+      {
+        id: 3,
+        img: './themes.svg',
+        name: "Contact Us"
+      },
+      {
+        id: 4,
+        img: './themes.svg',
+        name: "Blog"
+      },
+      {
+        id: 5,
+        img: './themes.svg',
+        name: "Whitepaper"
+      },
+      {
+        id: 6,
+        img: './themes.svg',
+        name: "Forum"
+      },
+      {
+        id: 7,
+        img: './themes.svg',
+        name: "Help Center"
+      }
+  ];
+  export default Resources;
+  
